7d3e8fe184683df275b231f03e8852f962bbd6f2
Chris Talkington <chris@talkingtontech.com>
2020-10-03 20:28:13 -0500

    Update conftest.py

--- tests/components/cloudflare/conftest.py
+++ tests/components/cloudflare/conftest.py
@@ -1,7 +1,8 @@
 """Define fixtures available for all tests."""
+from pycfdns import CFRecord
 from pytest import fixture
 
-from tests.async_mock import patch
+from tests.async_mock import AsyncMock, patch
 
 @fixture
 def cfupdate(hass):
@@ -10,6 +11,30 @@ def cfupdate(hass):
         "homeassistant.components.cloudflare.CloudflareUpdater"
     ) as mock_api:
         instance = mock_api.return_value
-        instance.get_zone_id.return_value = "mock-zone-id"
+
+        cf_records = [
+            CFRecord(
+                {
+                    "id": "zone-record-id",
+                    "type": "A",
+                    "name": "ha.mock.com",
+                    "proxied": True,
+                    "content": "127.0.0.1",
+                }
+            ),
+            CFRecord(
+                {
+                    "id": "zone-record-id-2",
+                    "type": "A",
+                    "name": "homeassistant.mock.com",
+                    "proxied": True,
+                    "content": "127.0.0.1",
+                }
+            )
+        ]
+
+        instance.get_record_info = AsyncMock(return_value=cf_records)
+        instance.get_zone_id = AsyncMock(return_value="mock-zone-id")
+        instance.update_records = AsyncMock(return_value=None)
 
         yield mock_api
