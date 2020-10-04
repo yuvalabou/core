298dd7ebd07f618dcf2fe56b88204f9d9278d078
Chris Talkington <chris@talkingtontech.com>
2020-10-03 19:35:42 -0500

    Update const.py

--- homeassistant/components/cloudflare/const.py
+++ homeassistant/components/cloudflare/const.py
@@ -8,5 +8,8 @@ CONF_RECORDS = "records"
 # Data
 DATA_UNDO_UPDATE_INTERVAL = "undo_update_interval"
 
+# Defaults
+DEFAULT_UPDATE_INTERVAL = 60 # in minutes
+
 # Services
 SERVICE_UPDATE_RECORDS = "update_records"
